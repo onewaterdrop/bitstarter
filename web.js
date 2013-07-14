b3f2e5f7cd5ec14fef5054b5f13920480e4101a3
onewaterdrop <osu.flyer2010@gmail.com>
2013-07-14 20:49:34 +0000

    changed web.js

--- web.js
+++ web.js
@@ -1,9 +1,20 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
+var buffer = new Buffer(256);
+
+
+
+fs.readFileSync('index.html',function(error,data){
+if(err) throw err;
+console.log(data);
+buffer.write(data,'utf-8');
+});
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(butter.toString());
+
 });
 
 var port = process.env.PORT || 5000;
